2af119527a6625685df94009ca3fd907030373b8
Alexandr Folomeshkin <fifax.adriano@gmail.com>
2021-04-27 22:52:33 +0600

    #1 Добавлена RegisterViewModel

--- HeadHunter/Controllers/AccountController.cs
+++ HeadHunter/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HeadHunter.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HeadHunter.Controllers
 {
     public class AccountController : Controller
     {
-
+        private readonly UserManager<User> _userManager;
     }
 }
---
+++ HeadHunter/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace HeadHunter.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Это поле обязательно")]
+        public string UserName { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно")]
+        [Display(Name = "Электронная почта")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно")]
+        [Display(Name = "Телефон")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+        
+        public string AvatarPath { get; set; }
+        public IFormFile File { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно")]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно")]
+        [Display(Name = "Введите пароль повторно")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+        
+    }
+}
