2cd91afa45df14ceeeb9cf09eb516d8f586a8be3
Alexandr Folomeshkin <fifax.adriano@gmail.com>
2021-05-07 20:10:12 +0600

    #6 Изменен Action публикации и снятия публикации вакансии

--- HeadHunter/Controllers/VacanciesController.cs
+++ HeadHunter/Controllers/VacanciesController.cs
@@ -153,6 +153,7 @@ namespace HeadHunter.Controllers
                 if (vacancy != null)
                 {
                     vacancy.Status = Status.Публичное;
+                    vacancy.DateOfPublication = DateTime.Now;
                     _db.Vacancies.Update(vacancy);
                     _db.SaveChanges();
                     return RedirectToAction("EmployerProfile", "Users", new {userId = vacancy.EmployerId});
@@ -170,6 +171,7 @@ namespace HeadHunter.Controllers
                 if (vacancy != null)
                 {
                     vacancy.Status = Status.Неопубликованное;
+                    vacancy.DateOfPublication = null;
                     _db.Vacancies.Update(vacancy);
                     _db.SaveChanges();
                     return RedirectToAction("EmployerProfile", "Users", new {userId = vacancy.EmployerId});
